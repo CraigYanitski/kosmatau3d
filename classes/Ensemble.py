a906ddbb837ef3e95095bf1496f5ef12741fddb6
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2019-10-31 09:43:50 +0100

    I have added most of the necessary functions for the emission calculation in Ensemble(), though I will still need to separate the Gauss(), Binomial(), and Poisson() classes.

--- classes/Ensemble.py
+++ classes/Ensemble.py
@@ -10,6 +10,8 @@ class Ensemble(object):
   def __init__(self, clumpType):
     self.__clumpType = clumpType     #type of mass in ensemble (clump or interclump medium)
     self.__combinations = []    #list of combinations
+    self.__combinationObjects = []    #list of instances of Combination()
+    self.__probability = []
     self.__intensity = 0        #ensemble-averaged intensity for each velocity
     self.__opticalDepth = 0     #ensemble-averaged optical depth for each velocity
     self.__FUV = 0
@@ -25,54 +27,108 @@ class Ensemble(object):
     self.__masspointNumberRange = []
     self.__masspointRadii = []
     self.__interpolationPoints = []
+    self.__deltaNji = []
     return
   def __setMass(self, mass):
+    '''Set the mass.'''
     self.__mass = mass
     return
 
   # PUBLIC
   def setMass(self, mass):
+    '''Set the mass.'''
     self.__setMass(mass)
     return
   def calculateMasspoints(self):
+    '''This is a function to get the clump masses in this ensemble. It will soon be depreciated
+       as I will change the reading of the KOSMA-tau files to convert from the fortran 10*log(values).'''
     self.__masspoints = np.arange(self.__massLimitLower, self.__massLimitUpper, 10)
     return
-  def calculateSurfaceDensity(self):
-    return np.log10(self.__surfaceDensity)
+  def calculateMasspointDensity(self):
+    '''I am not entirely certain this is needed...'''
+    return np.log10(self.__masspointDensity)
   def getInterpolationPoints(self):
-
+    '''This might be removed. I'm not certain the interpolation points need to be accessed out-of-class.'''
+    return
   def getMasspoints(self):
+    '''This is pretty self-explanatory. Return the list of clump masses when called...'''
     return self.__masspoints
   def calculateRadii(self):
-    self.__surfaceDensity = 10**(self.__masspoints/10.*(1-3/gbl._globals['constants']['gamma']))*sum(10**(self.__masspoints/10.**(1+3/gbl._globals['constants']['gamma']-gbl._globals['constants']['alpha']))) / \
-                            sum(10**(self.__masspoints/10.0*(2-gbl._globals['constants']['alpha'])))*rho_ens/1.91
-    if (self.__surfaceDensity>10**(gbl._globals['compound']['ranges']['max_arr'][0]/10)).any() or (self.__surfaceDensity<10**(gbl._globals['compound']['ranges']['min_arr'][0]/10)).any(): sys.exit('WARNING: surface density outside of KOSMA-tau grid!\n\n')
-    self.__interpolationPoints = np.stack((self.__surfaceDensity, self.__masspoints, np.full(self.__masspoints.size, self.__FUV)))
-    self.__masspointRadii = ((3/(4.0*np.pi)*(10**(self.__masspoints/10.0)*gbl._globals['constants']['M_sol'])/(10**(self.__interpolationPoints/10.0)*gbl._globals['constants']['M_H']*1.91))**(1./3.))/gbl._globals['constants']['pc']
+    '''This function calculates the interpolation points necessary for reading the KOSMA-tau files.'''
+    self.__masspointDensity = self.__masspoints**(1-3/gbl._globals['constants']['gamma'])*sum(self.__masspoints**(1+3/gbl._globals['constants']['gamma']-gbl._globals['constants']['alpha'])) / \
+                            sum(self.__masspoints**(2-gbl._globals['constants']['alpha']))*self.__density/1.91
+    if (self.__masspointDensity>gbl._globals['compound']['ranges']['max_arr'][0]).any() or (self.__masspointDensity<gbl._globals['compound']['ranges']['min_arr'][0]).any(): sys.exit('WARNING: surface density outside of KOSMA-tau grid!\n\n')
+    self.__interpolationPoints = np.stack((self.__masspointDensity, self.__masspoints, np.full(self.__masspoints.size, self.__FUV)))
+    self.__masspointRadii = ((3/(4.0*np.pi)*(self.__masspoints*gbl._globals['constants']['M_sol'])/(self.__interpolationPoints[0]*gbl._globals['constants']['M_H']*1.91))**(1./3.))/gbl._globals['constants']['pc']
+    return
+  def calculateCombinations(self):
+    '''This function calculates all of the different combinations of clump masses that may be in a line-of-sight.
+      It is basically the essence of the probabilistic approach used to create the superposition of clumps.'''
+    list = []
+    for line in self.__masspointNumberRange:
+      l = np.arange(line[0], line[1] + 1)
+      list.append(l)
+    self.__combinations = np.zeros([self.numbercombi, self.numberintervals]) 
+    for j in range(np.array(self.__masspointNumberRange).prod(1).sum()): # each combination
+      for i in range(len(self.__masspointNumberRange)): # each column 
+        prod = 1
+        while a < (len(self.numberintervals) - 1):
+          prod = prod * self.list[a+1].size
+          a = a + 1
+        self.__combinations[j][i] = int(j/(prod)) % self.list[i].size + self.list[i][0]
     return
   def getCombinations(self):
-    for i,mass in self.getMasspoints:
-      number = ((Mens*(10**(M_log/10.0))**(1-gbl._globals['constants']['alpha']))/(sum((10**(M_log2/10.0))**(2-gbl._globals['constants']['alpha']) for M_log2 in MLogArray)))
-      pTab = (np.pi*self.radius**2/constants.pixelWidth**2)
-      expectedValTab = (number_v*pTab)
-      standardDeriTab = (np.sqrt(number_v*pTab*(1-pTab)))
-      lower = max(0, np.floor(expectedValTab-gbl._globals['constants']['nsigma']*standardDeriTab))
-      upper = min(number_v, np.ceil(expectedValTab+gbl._globals['constants']['nsigma']*standardDeriTab))
-      self.__masspointNumberRange.append([lower, upper])
-    combinations = self.calculateCombinations(self.__masspointNumberRange)
-    for combination in combinations: self.__combinations.append(Combination(combination))
+    '''This function removes all of the unnecessary degenerate looping during this calculation.
+       Of course it is possible because of the wonders of nump.ndarray(). . .'''
+    self.__deltaNji = (self.__mass*(self.__masspoints)**(1-gbl._globals['constants']['alpha'])) / sum((self.__masspoints)**(2-gbl._globals['constants']['alpha']))
+    pTab = (np.pi*self.__masspointRadii**2/constants.pixelWidth**2)
+    expectedValTab = (self.__deltaNji*pTab)
+    standardDeriTab = (np.sqrt(self.__deltaNji*pTab*(1-pTab)))
+    lower = np.maximum(np.zeros(np.size(self.__masspoints)), np.floor(expectedValTab-gbl._globals['constants']['nsigma']*standardDeriTab))
+    upper = np.minimum(self.__deltaNji, np.ceil(expectedValTab+gbl._globals['constants']['nsigma']*standardDeriTab))
+    self.__masspointNumberRange = np.array([lower, upper]).T
+    self.calculateCombinations()
+    if self.flagBinomialPoisson:
+      if np.any(expectedValTab>gbl._globals['constants']['pn_gauss'] and number>gbl._globals['constants']['N_gauss']):
+        # use gauss!
+        g = Gauss(expectedValTab[ma], standardDeriTab[ma])
+        self.__probability.append(g.gaussfunc)
+        pause = input('gauss!!...')
+      else:
+        # use binomial 
+        b = Binomial(number_v[ma], pTab[ma]) # n and p for binominal 
+        self.__probability.append(b.binomfunc)
+    else:
+      if np.any(expectedValTab>gbl._globals['constants']['pn_gauss'] and number>gbl._globals['constants']['N_gauss']):
+        # use gauss
+        g = Gauss(expectedValTab[ma], standardDeriTab[ma])
+        self.__probability.append(g.gaussfunc)
+        pause = input('gauss!!...')
+      else:
+        # use poisson
+        po = Poisson(expectedValTab[ma])
+        self.__probability.append(po.poissonfunc)
     return
   def calculate(self):
+    '''Maybe <<PARALLELISE>> this??
+
+       This is a function to cycle through the Combination instances to create a large numpy.ndarray,
+       which is used to calculate the final sums needed for the voxel.'''
     self.calculateMasspoints()
     self.getCombinations()
+    combinations = []
+    result = []
     for combination in self.__combinations:
-      combination.calculateEmission()
-      result = combination.getScaledCombinationEmission()
-      self.__intensity += result[0]*result[1]
-      self.__opticalDepth += result[0]*np.exp(-result[2])
-      self.__FUV += result[0]*np.exp(-result[3])
+      self.__combinationObjects.append(Combination(combination, self.__masspoints, probability=self.__probability))
+      self.__combinationObjects[-1].calculateEmission()
+      result.append(self.__combinationObjects[-1].getScaledCombinationEmission())
+    result = np.array(result)
+    self.__intensity = result.sum(3)[0]
+    self.__opticalDepth = result.sum(3)[1]
+    self.__FUV = result.sum(3)[2]
     return
   def getEnsembleEmission(self):
+    '''This returns the ensemble emission...nothing more.'''
     return (self.__intensity,self.__opticalDepth,self.__FUV)
 
 class binomial():
@@ -141,3 +197,12 @@ class gauss:
       import numpy as np   
       return self.area/(np.sqrt(2*np.pi)*self.sigma)*\
              np.exp(-.5 * ( (v - self.v0) / self.sigma)**2 )
+
+class poisson():
+  def __init__(self, la):
+    self.la = float(la) # lambda = n * p
+    return
+  
+  def poissonfunc(self, k):
+    # print 'comb', self.comb(k)
+    return self.la**k /factorial(k) * np.e**(-self.la)
