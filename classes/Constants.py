b314cda9577a5e4fd568b53fd059f33f7f1d64a7
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2019-11-01 09:30:25 +0100

    The Constants() class has been demoted to just a file containing constants.

--- classes/Constants.py
+++ classes/Constants.py
@@ -1,9 +1,9 @@
-class Constants():
+#class Constants():
   '''
   This is a class to contain all of the constants used throughout the program.
   '''
   # PRIVATE
-  def __init__(self, interpolation='linear', clumpMassRange=[10,10], interclumpMassRange=[0.1,0.1], alpha=1.84, gamma=2.31):
+#  def __init__(self, interpolation='linear', clumpMassRange=[10,10], interclumpMassRange=[0.1,0.1], alpha=1.84, gamma=2.31):
     self.clumpMassUpper = clumpMassRange[0]
   	self.clumpMassLower = clumpMassRange[1]
     self.interclumpMassUpper = interclumpMassRange[0]
@@ -15,4 +15,4 @@ class Constants():
     self.c = 2.998*10**8  #in [m/s]
     self.kB = 1.3806*10**-23  #in [J/K]
     self.pc = 3.0856776*10**16  #in [m]
-    return
+#    return
