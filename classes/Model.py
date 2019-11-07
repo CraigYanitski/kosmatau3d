8f0d2c8e9e0f1e779fad932321d8c8c6f7cb369e
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2019-11-07 17:53:17 +0100

    The voxels are now properly created.

--- classes/Model.py
+++ classes/Model.py
@@ -83,122 +83,22 @@ class Model():
     #self.__species = [self.__molecules, self.__dust]
     self.__speciesNames = np.append(self.__molecules.getMolecules(), self.__dust.getDust())
     return
-  def addSpecies(self, species, transition):
+  def addSpecies(self, speciesTransition):
     # find transition number as defined in
     # SetUpOrionBarModelEnvironment.v.1.1.nb
     #from PDR import _globals  
     #(number,species,transition,frequency) = self.__observations.__speciesData
     #gbl._globals['compound']['number'] = []
     #gbl._globals['compound']['frequency'] = []
-    for element in species:
+    for species in speciesTransition:
+      element = species.split(' ')[0]
+      transition = int(species.split(' ')[1])
       if element=='Dust':
-        self.addDust(species, transition)
+        self.addDust(element, transition)
         return
       else:
-        self.addMolecule(species, transition)
+        self.addMolecule(element, transition)
         return
-
-
-
-
-
-
-
-  #   for i in range(gbl._globals['compound']['nspe']):
-  #     # for each species 
-  #     number = -1
-  #     species = gbl._globals['compound']['species'][i]
-  #     transition = gbl._globals['compound']['transition'][i]
-  #     if species == 'C+': number = 1
-  #     elif species == 'C':  
-  #       if transition > 3 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 1 + transition
-  #     elif species == 'O':
-  #       if transition > 3 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 4 + transition
-  #     elif species == 'CO': 
-  #       if transition > 49 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 7 + transition
-  #     elif species == '13CO': 
-  #       if transition > 49 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 56 + transition
-  #     elif species == '13C+': number = 106
-  #     elif species == '13C':
-  #       if transition > 3 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 106 + transition
-  #     elif species == 'HCO+':
-  #       if transition > 15 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 109 + transition
-  #     elif species == 'H13CO+':
-  #       if transition > 30 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 124 + transition
-  #     elif species == 'H3O+':
-  #       if transition > 17 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 154 + transition
-  #     elif species == 'C18O':
-  #       if transition > 49 or transition < 1:
-  #         import sys    
-  #         sys.exit('transition does not exist...exiting...')
-  #       else: number = 171 + transition
-  #     elif species == 'Dust':
-  #       if transition > 51 or transition < 1:
-  #         import sys
-  #         sys.exit('PDR_functions:this Dust transition is not implemented...exiting...')
-  #       else: number = 191 + transition
-  # #    elif species == 'CH+': 
-  # #        if transition > 10 or transition < 1:
-  # #            import sys    
-  # #            sys.exit('transition does not exist...exiting...')
-  # #        else: number = 171 + transition
-  #     if number == -1:
-  #       print('species: ', species)
-  #       print('transition: ', transition)
-  #       import sys    
-  #       sys.exit('species/transition combination does not exist...exiting...')            
-  #     gbl._globals['compound']['number'].append(number) 
-  #     with open(gbl.INPUTPATH+'frequencies.dat','r') as f:
-  #       for i, line in enumerate(f):
-  #         if i == number:
-  #           try: freq = float(line.split()[3]) 
-  #           # read frequency from frequency.dat    
-  #           except IndexError:
-  #             import sys
-  #             sys.exit('Problem :( frequency information needs to be added to file frequencies.dat ...exiting...')          
-  #           gbl._globals['compound']['frequency'].append(freq)
-  #           #print 'hier'
-            
-  #         #elif i > number:
-  #           #print 'break'
-  #           #break
-  #     #gbl._globals['compound']['frequency'].append(freq)
-  #   if len(gbl._globals['compound']['transition']) != len( gbl._globals['compound']['frequency']):
-  #     print('frequencies found ' , len( gbl._globals['compound']['frequency']))
-  #     print('frequencies seeked ', len(gbl._globals['compound']['transition']))
-  #     import sys
-  #     sys.exit('Problem :( Frequency information does not match. Maybe needs to be added to file frequencies.dat ...exiting...')
-    
-  #     # print 'number', gbl._globals['compound']['number']
-  #     # print 'frequency',  gbl._globals['compound']['frequency'] 
-  #     # pause = input('..ok?..remove again..in PDRfunctions..')
-  #     return gbl._globals 
-  #       #gbl._globals['compound']['frequency'].append(freq)
-  #   # print 'number', gbl._globals['compound']['number']
-  #   # print 'frequency',  gbl._globals['compound']['frequency'] 
-  #   # pause = input('..ok?..remove again..in PDRfunctions..')
-  #   return gbl._globals
+  def calculateEmission(self):
+    self.__grid.calculateEmission()
+    return
