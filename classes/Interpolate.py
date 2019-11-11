The interpolation of the rotation profile was corrected. The input file also includes the velocity dispersion, so this has also been added.

classes/Interpolate.py
@@ -24,6 +24,7 @@ class Interpolate(object):
     self.__verbose = verbose
     self.__intensityInterpolation,self.__tauInterpolation = self.__calculateGridInterpolation()
     self.__rotationInterpolation = self.__calculateRotationVelocity()
+    self.__dispersionInterpolation = self.__calculateVelocityDispersion()
     self.__densityInterpolation = self.__calculateDensity()
     self.__clumpMassInterpolation = self.__clumpMassProfile()
     self.__interclumpMassInterpolation = self.__interclumpMassProfile()
@@ -58,10 +59,18 @@ class Interpolate(object):
     if self.__verbose: print('Creating rotation velocity interpolation')
     rotation = self.__observations.rotationProfile 
     if self.__interpolation=='linear':
-      return interpolate.interp1d(rotation[0], rotation[1], kind='linear')    #rotation velocity interpolation
+      return interpolate.interp1d(rotation[0], rotation[1][:,0], kind='linear')    #rotation velocity interpolation
     if self.__interpolation=='cubic' or self.__interpolation=='radial':
-      return interpolate.interp1d(rotation[0], rotation[1], kind='cubic')    #rotation velocity interpolation
-    else: sys.exit('<<ERROR>>: There is no such method as {} to interpolate the KOSMA-tau grid.\n\nExitting...\n\n'.format(self.__interpolation))
+      return interpolate.interp1d(rotation[0], rotation[1][:,0], kind='cubic')    #rotation velocity interpolation
+    else: sys.exit('<<ERROR>>: There is no such method as {} to interpolate the velocity profile.\n\nExitting...\n\n'.format(self.__interpolation))
+  def __calculateVelocityDispersion(self):
+    if self.__verbose: print('Creating velocity dispersion interpolation')
+    rotation = self.__observations.rotationProfile 
+    if self.__interpolation=='linear':
+      return interpolate.interp1d(rotation[0], rotation[1][:,1], kind='linear')    #rotation velocity interpolation
+    if self.__interpolation=='cubic' or self.__interpolation=='radial':
+      return interpolate.interp1d(rotation[0], rotation[1][:,1], kind='cubic')    #rotation velocity interpolation
+    else: sys.exit('<<ERROR>>: There is no such method as {} to interpolate the velocity profile.\n\nExitting...\n\n'.format(self.__interpolation))
   def __calculateDensity(self):
     if self.__verbose: print('Creating density interpolation')
     density = self.__observations.densityProfile
@@ -134,6 +143,8 @@ class Interpolate(object):
     return (np.array(tau)).sum(0)
   def interpolateRotationalVelocity(self, radius):
     return self.__rotationInterpolation(radius)
+  def interpolateVelocityDispersion(self, radius):
+    return self.__dispersionInterpolation(radius)
   def interpolateDensity(self, radius):
     return self.__densityInterpolation(radius)
   def interpolateClumpMass(self, radius):

classes/Observations.py
@@ -8,8 +8,8 @@ class Observations(object):
   # PRIVATE
   def __init__(self, scale, directory='MilkyWay'):
     self.__scale = scale
-    self.__INPUTPATH = '/home/yanitski/Desktop/KOSMA-tau^3/input/'
-    self.__GRIDPATH = '/home/yanitski/Desktop/KOSMA-tau^3/grid/'
+    self.__INPUTPATH = '/home/craig/Desktop/Köln/kosma-tau^3-develop/kosma-tau-3d/input/'#'/home/yanitski/Desktop/KOSMA-tau^3/input/'#
+    self.__GRIDPATH = '/home/craig/Desktop/Köln/kosma-tau^3-develop/kosma-tau-3d/grid/'#'/home/yanitski/Desktop/KOSMA-tau^3/grid/'#
     if directory[-1]=='/': self.__directory = directory
     else: self.__directory = directory + '/'
     self.__initialise()
@@ -34,8 +34,8 @@ class Observations(object):
     return (fuv[:,0],fuv[:,1],fuv[:,2:])
   def __rotationProfile(self, file='rot_milki2018_14.dat'):
     # Open file for the rotation profile of the object
-    rotation = np.genfromtxt(self.__INPUTPATH+self.__directory+file, names=['radius', 'rotation_velocity'])
-    return (rotation['radius']*1000,rotation['rotation_velocity'])
+    rotation = np.genfromtxt(self.__INPUTPATH+self.__directory+file)
+    return (rotation[:,0]*1000., rotation[:,1:])
   def __tauCenterline(self, file='tau_linecentre.dat'):
     # Open file for KOSMA-tau simulations of optical depths
     # FORMAT: n, M, UV, tau[molecules then dust]