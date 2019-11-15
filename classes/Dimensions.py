The FUV calculation is now implemented correctly. There was an old error in the geometry calculation, so that has also been fixed.

classes/Dimensions.py
@@ -29,7 +29,7 @@ class Dimensions(object):
     return
   def __convertToPolar(self):
     # This is a function that calculates the polar coordinates of each voxel
-    r = ((self.__xPositions-self.__xOffset)**2 + (self.__yPositions-self.__yOffset)**2)**0.5
+    r = ((self.__xPositions)**2 + (self.__yPositions)**2)**0.5
     phi = 0#np.arcsin(self.__yPositions/self.__xPositions)
     return r, phi
   def __str__(self):

classes/Voxel.py
@@ -53,7 +53,7 @@ class Voxel(object):
     return
   def __setFUV(self):
     fuv = self.__interpolations.interpolateFUVfield(self.__r)/self.__constants.normUV*self.__constants.globalUV
-    fuv = 10**(np.clip(fuv, 1, None))
+    fuv = np.clip(fuv, 1, None)
     self.__FUV = FUVfield(fuv)
     return
   def __str__(self):