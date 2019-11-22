bb4cf2256bf5cc1d952f0146af87746e5e6db569
CraigYanitski <cyanitski@gmail.com>
2019-11-22 19:29:51 +0100

    Some velocity dispersion values were added to properly compare to the old code.

--- classes/Constants.py
+++ classes/Constants.py
@@ -3,7 +3,7 @@ class Constants(object):
   This is a class to contain all of the constants used throughout the program.
   '''
   #PRIVATE
-  def __init__(self, clumpMassRange=[1,3], interclumpMassRange=[-2,-1], pixelWidth=1000, alpha=1.84, gamma=2.31, nsigma=3, nGauss=1000, pnGauss=5):
+  def __init__(self, clumpMassRange=[1,3], interclumpMassRange=[-2,-1], pixelWidth=1000, alpha=1.84, gamma=2.31, nsigma=3, nGauss=1000, pnGauss=5, clumpDispersion=1.67/2.3548, ensembleDispersion=10./2.3548):
     self.clumpMassLimits = clumpMassRange
     self.interclumpMassLimits = interclumpMassRange
     self.alpha = alpha
@@ -12,6 +12,8 @@ class Constants(object):
     self.nGauss = nGauss
     self.pnGauss = pnGauss
     self.pixelWidth = pixelWidth  #in pc
+    self.clumpDispersion = clumpDispersion
+    self.ensembleDispersion = ensembleDispersion
     self.massH = 1.008*1.6605*10**-24  #in [g]
     self.massSolar = 1.998892*10**33  #in [g]
     self.c = 2.998*10**10  #in [cm/s]
