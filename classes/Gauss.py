4f1824c3c52a0f4ab040c9889915e8c3de5505d6
CraigYanitski <cyanitski@gmail.com>
2019-12-05 17:33:58 +0100

    The Gaussian probability is now adjusted to behave correctly with an array argument. This was done by including an array of the indeces where there is no sigma value, thus isolating what needs to be calculated and what just has a probability of 0.

--- classes/Gauss.py
+++ classes/Gauss.py
@@ -1,15 +1,21 @@
 import numpy as np
 class Gauss:
   '''This is a classs taken directly from the work of Silke Andree-Labsch and Christoph Bruckmann.'''
-  def __init__(self, v0, sigma, area = 1):
+  def __init__(self, v0, sigma, area=1, debug=False):
+    self.debug = debug
     self.area = np.array(area) # area below the curve (integrated curve) 
     self.v0 = np.array(v0) # peak velocity
     self.sigma = np.array(sigma) # standard deviation
+    self.sigmaIndeces = self.sigma==0
     return
   
   def gaussfunc(self, v):
-    if np.any(np.array(self.sigma)==0):
-      return 0
-    else:
-      power = np.array(-.5*np.power((v-self.v0)/self.sigma, 2))
-      return self.area/(np.sqrt(2*np.pi)*self.sigma)*2.718281818459**(power)
+    #if np.any(np.array(self.sigma)==0):
+    #  return 0
+    #else:
+    power = np.array(-.5*((v-self.v0)/self.sigma)**2)
+    probability = self.area/(np.sqrt(2*np.pi)*self.sigma)*np.exp(power)
+    probability[self.sigmaIndeces] = 0
+    probability = probability.T
+    if self.debug: input('\nProbability:\n{}'.format(probability))
+    return probability
