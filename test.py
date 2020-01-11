3c0a869003b13722e5b663c7a8b9a0165d406001
CraigYanitski <cyanitski@gmail.com>
2020-01-11 11:58:17 +0100

    The integrated intensity map is now created and stored in two variables.

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ kosma = Model(12, 12, 3,modelType='disk',resolution=1000)
 kosma.addSpecies(species)
 kosma.initialiseModel()
 kosma.calculateEmission()
-for voxel in kosma.getGrid().allVoxels():
-  position = voxel.getPosition()
-  print('Voxel {} at position x={}, y={}, z={}.'.format(voxel.getIndex(), position[0], position[1], position[2]))
+#for voxel in kosma.getGrid().allVoxels():
+#  position = voxel.getPosition()
+#  print('Voxel {} at position x={}, y={}, z={}.'.format(voxel.getIndex(), position[0], position[1], position[2]))
 #kosma.setLOS()
-print(kosma.calculateObservation())
+position,intensity = kosma.calculateObservation()
+#print('at position {}, the intensity is:\n{}\n'.format(position[50],intensity[50]))
 #kosma.getGrid().printVoxels()
 ##
 #for i in range(kosma.getGrid().getVoxelNumber()): print(i, ':', kosma.getGrid().allVoxels()[i].getPosition())
