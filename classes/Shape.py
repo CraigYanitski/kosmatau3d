8cc2eb829782a69bd13dde37baa0ef5dc96fa396
CraigYanitski <cyanitski@gmail.com>
2020-01-15 23:29:54 +0100

    Functions have been added to return the voxel coordinates.

--- classes/Shape.py
+++ classes/Shape.py
@@ -15,8 +15,51 @@ class Shape():
     self.__z = []
     self.__r = []
     self.__phi = []
-    if self.__type=='disk': createDisk()
-    if self.__type=='spheroid': createSpheroid()
+    if self.__type=='disk': self.__createDisk()
+    if self.__type=='spheroid': self.__createSpheroid()
+    return
+  def __createDisk(self):
+    x,y,z = self.__dimensions.voxelCartesianPosition()
+    r,phi = self.__dimensions.voxelPolarPosition()
+    X = []
+    Y = []
+    Z = []
+    R = []
+    PHI = []
+    print(x.max)
+    for i in range(len(r)):
+      if np.sqrt(x[i]**2+y[i]**2)<max(x):
+        X.append(x[i])
+        Y.append(y[i])
+        Z.append(z[i])
+        R.append(r[i])
+        PHI.append(phi[i])
+    self.__x = np.array(X)
+    self.__y = np.array(Y)
+    self.__z = np.array(Z)
+    self.__r = np.array(R)
+    self.__phi = np.array(PHI)
+    return
+  def __createSpheroid(self):
+    x,y,z = self.__dimensions.voxelCartesianPosition()
+    r,phi = self.__dimensions.voxelPolarPosition()
+    X = []
+    Y = []
+    Z = []
+    R = []
+    PHI = []
+    for i in range(len(r)):
+      if (x[i]/(x))**2+(y[i]/(y))**2+(z[i]/(z))**2<1:
+        X.append(x[i])
+        Y.append(y[i])
+        Z.append(z[i])
+        R.append(r[i])
+        PHI.append(phi[i])
+    self.__x = np.array(X)
+    self.__y = np.array(Y)
+    self.__z = np.array(Z)
+    self.__r = np.array(R)
+    self.__phi = np.array(PHI)
     return
 
   # PUBLIC
@@ -32,47 +75,9 @@ class Shape():
   def voxelNumber(self):
     return self.__r.size
   #def setup(self, x=0, y=0, z=0, r=0, i=0, name='box'):
-
-  def createDisk(self):
-    x,y,z = self.__dimensions.voxelCartesianPosition()
-    r,phi = self.__dimensions.voxelPolarPosition()
-    X = []
-    Y = []
-    Z = []
-    R = []
-    PHI = []
-    for i in range(len(r)):
-      if np.sqrt(x[i]**2+y[i]**2)<x.max:
-        X.append(x[i])
-        Y.append(y[i])
-        Z.append(z[i])
-        R.append(r[i])
-        PHI.append(phi[i])
-    self.__x = np.array(X)
-    self.__y = np.array(Y)
-    self.__z = np.array(Z)
-    self.__r = np.array(R)
-    self.__phi = np.array(PHI)
-    return
-
-  def createSpheroid(self):
-    x,y,z = self.__dimensions.voxelCartesianPosition()
-    r,phi = self.__dimensions.voxelPolarPosition()
-    X = []
-    Y = []
-    Z = []
-    R = []
-    PHI = []
-    for i in range(len(r)):
-      if (x[i]/x.max)**2+(y[i]/y.max)**2+(z[i]/z.max)**2<1:
-        X.append(x[i])
-        Y.append(y[i])
-        Z.append(z[i])
-        R.append(r[i])
-        PHI.append(phi[i])
-    self.__x = np.array(X)
-    self.__y = np.array(Y)
-    self.__z = np.array(Z)
-    self.__r = np.array(R)
-    self.__phi = np.array(PHI)
-    return
+  def voxelCartesianPositions(self):
+    # Return the Cartesian coordinates of each voxel
+    return (self.__x, self.__y, self.__z, self.__scale)
+  def voxelPolarPositions(self):
+    # Return the polar coordinates of each voxel
+    return (self.__r, self.__phi)
