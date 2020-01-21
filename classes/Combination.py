The FUV extinction calculation was fixed.

classes/Combination.py
@@ -54,8 +54,8 @@ class Combination(object):
   def getAfuv(self):
     Afuv = 0.
     for i in range(len(self.__masspoints)):
-      Afuv += masspoint.getAfuv()
-    return self.__probability*self.__combination*np.exp(-Afuv)
+      Afuv += self.__combination[i]*self.__masspoints[i].getAfuv()
+    return self.__probability.max()*np.exp(-Afuv)
   def addMasspoint(self, mass, number):
     self.__masspoints.append(Masspoint(self.__species, self.__interpolations, mass, number))
     self.__combination.append(number)

classes/Ensemble.py
@@ -98,8 +98,8 @@ class Ensemble(object):
     self.__setVelocityDispersion(velocityDispersion)
     self.__setExtinction(extinction)
     self.__setFUV(FUV)
-    self.initialiseEnsemble()
-    return
+    Afuv = self.initialiseEnsemble()
+    return Afuv
   def setMass(self, mass):
     '''Set the mass.'''
     self.__setMass(mass)
@@ -321,7 +321,7 @@ class Ensemble(object):
     self.calculateRadii()
     Afuv = self.createCombinationObjects()
     if self.__verbose: print(self.__clumpType)
-    return np.log(Afuv)
+    return Afuv
   #@jit(nopython=False)
   def calculate(self, Afuv, debug=False, test=False):
     '''Maybe <<PARALLELISE>> this??