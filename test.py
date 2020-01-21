a86b1746acff0252608a2b8cf59a65147f434e12
CraigYanitski <cyanitski@gmail.com>
2020-01-21 13:43:24 +0100

    A few lines were added to plot the 3D model, coloured according to its intensity.

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import inspect
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 pathname = inspect.getframeinfo(inspect.currentframe()).filename
 print(os.path.dirname(pathname))
 sys.path.append(os.path.abspath(os.path.dirname(pathname))+'/classes/')
 from Model import *
 species = ['13CO 10', 'C+ 1', 'CO 1']#, '13CO 1', 'O 2', 'Dust 1', 'Dust 2', 'Dust 3', 'Dust 4', 'Dust 5']
-kosma = Model(6, 6, 0,modelType='disk',resolution=100)
+kosma = Model(36, 36, 3,modelType='disk',resolution=500)
 kosma.addSpecies(species)
 kosma.initialiseModel()
 kosma.calculateEmission()
@@ -15,8 +17,15 @@ kosma.calculateEmission()
 #  print('Voxel {} at position x={}, y={}, z={}.'.format(voxel.getIndex(), position[0], position[1], position[2]))
 #kosma.setLOS()
 kosma.calculateObservation()
-kosma.printIntensityMap()
-intensity = kosma.getIntensityMap()
+#kosma.printIntensityMap()
+positions, intensity = kosma.getIntensityMap()
+positions = positions.T
+positions = kosma.getGrid().getVoxelPositions()
+intensity = kosma.getGrid().totalEmission()[0]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(positions[0], positions[1], positions[2], c=(intensity.max(2)).sum(1)/intensity.max(), cmap=plt.cm.jet)
+plt.show()
 #print('at position {}, the intensity is:\n{}\n'.format(position[50],intensity[50]))
 #kosma.getGrid().printVoxels()
 ##
