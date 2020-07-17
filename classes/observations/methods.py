90e3f38b7457138d3597afe16c0c10b5d99f409b
yanitski <yanitski@ph1.uni-koeln.de>
2020-07-18 00:18:20 +0200

    [update] adjusted model-specific methods to not evaluate if there is no model specified

--- classes/observations/methods.py
+++ classes/observations/methods.py
@@ -28,37 +28,42 @@ def initRadTransfer():
 
 def clumpMassProfile(file='mass_profile.dat'):
   # Open file for the mass profile (clump) of the object (Msol/pc**2)
-  clumpMass = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_mass'])
-  observations.clumpMassProfile = (clumpMass['radius']*1000,clumpMass['h2_mass']*constants.resolution**2)
+  if constants.directory!='':
+    clumpMass = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_mass'])
+    observations.clumpMassProfile = (clumpMass['radius']*1000,clumpMass['h2_mass']*constants.resolution**2)
   return
 
 def interclumpMassProfile(file='mass_profile_inter.dat'):
   # Open file for the mass profile (interclump) of the object (Msol/pc**2)
-  interclumpMass = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_mass'])
-  observations.interclumpMassProfile = (interclumpMass['radius']*1000,interclumpMass['h2_mass']*constants.resolution**2)
+  if constants.directory!='':
+    interclumpMass = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_mass'])
+    observations.interclumpMassProfile = (interclumpMass['radius']*1000,interclumpMass['h2_mass']*constants.resolution**2)
   return
 
 def densityProfile(file='densities_clouds.dat'):
   # Open file for the number density profile of the object (n/cm**3)
-  density = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_density'])
-  observations.densityProfile = ((density['radius']*1000),density['h2_density'])
+  if constants.directory!='':
+    density = np.genfromtxt(constants.INPUTPATH+constants.directory+file, names=['radius', 'h2_density'])
+    observations.densityProfile = ((density['radius']*1000),density['h2_density'])
   return
 
 def FUVfield(file='galactic_FUV_complete.dat'):
   '''Open file for the FUV profile of the object
      'radius', 'energy density 912', 'energy density 1350', 'energy density 1500', 'energy density 1650', 'energy density 2000', \
      'energy density 2200', 'energy density 2500', 'energy density 2800', 'energy density 3650'])'''
-  fuv = np.loadtxt(constants.INPUTPATH+constants.directory+file)
-  #r = np.arange(0,20000,50)
-  #fuv = 50/4/np.pi/r**2
-  observations.FUVfield = (fuv[:,:2],constants.FUVFactor*fuv[:,2],fuv[:,3:])
-  #return (r,fuv)
+  if constants.directory!='':
+    fuv = np.loadtxt(constants.INPUTPATH+constants.directory+file)
+    #r = np.arange(0,20000,50)
+    #fuv = 50/4/np.pi/r**2
+    observations.FUVfield = (fuv[:,:2],constants.FUVFactor*fuv[:,2],fuv[:,3:])
+    #return (r,fuv)
   return
 
 def rotationProfile(file='rot_milki2018_14.dat'):
   # Open file for the rotation profile of the object
-  rotation = np.genfromtxt(constants.INPUTPATH+constants.directory+file)
-  observations.rotationProfile = (rotation[:,0]*1000., rotation[:,1:])
+  if constants.directory!='':
+    rotation = np.genfromtxt(constants.INPUTPATH+constants.directory+file)
+    observations.rotationProfile = (rotation[:,0]*1000., rotation[:,1:])
   return
 
 def tauCenterline(file='tau_linecentre.dat'):
