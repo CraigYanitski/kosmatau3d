76cec7903780dc98fce5e4da37de656673407c3b
yanitski <yanitski@ph1.uni-koeln.de>
2020-11-27 01:56:06 +0100

    [delete] removed some of the unnecessary files

--- kosmatau3d/models/ensembleStatistics/__init__.py
+++
@@ -1,9 +0,0 @@
-import math
-import numpy as np
-import scipy.special
-import functools as ft
-from operator import mul
-
-from .binomial import Binomial
-from .gauss import Gauss
-from .poisson import Poisson
