18b12399d99140d41481216863de2952f4ca937c
yanitski <yanitski@ph1.uni-koeln.de>
2021-02-04 10:58:27 +0100

    [update] the plotting class now produces a proper application window

--- kosmatau3d/models/plotting/viewMap.py
+++ kosmatau3d/models/plotting/viewMap.py
@@ -1,6 +1,7 @@
 import sys
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from astropy.io import fits
 import numpy as np
 from PyQt5.QtCore import Qt
@@ -27,29 +28,33 @@ matplotlib.use('Qt5Agg')
 def main():
   directory = constants.HISTORYPATH
   
-  app = QApplication(sys.argv)
+  # app = QApplication()
   
   window = viewMap(directory=directory)
-  sys.exit(app.exec_())
+  
+  sys.exit(window.exec_())
 
-class viewMap(QWidget):
-  fig = plt.figure(figsize=(9, 6))
-  def __init__(self, directory, posXY=(700, 40), windowSize=(925, 750)):
-    super().__init__()
+class viewMap(QApplication):
+  
+  def __init__(self, directory='', posXY=(700, 40), windowSize=(925, 750)):
+    if not directory:
+      print('please specify the directory of the model history files.')
+    super().__init__(sys.argv)
+    self.window = QWidget()
     self.title = 'Subplot Switcher'
     self.posXY = posXY
     self.windowSize = windowSize
     self.directory = directory
     self.file = fits.open(directory+'/channel_intensity.fits')
-    velocity = np.linspace(self.file[1].header['CRVAL4'] - self.file[1].header['CRPIX4'] * self.file[1].header['CDELT4'],
+    self.velocity = np.linspace(self.file[1].header['CRVAL4'] - self.file[1].header['CRPIX4'] * self.file[1].header['CDELT4'],
                            self.file[1].header['CRVAL4'] + self.file[1].header['CRPIX4'] * self.file[1].header['CDELT4'],
                            num=self.file[1].header['NAXIS4'])
-    self.species_intensity_integrated = np.trapz(self.file[1].data, velocity, axis=0)
-    self.dust_intensity_integrated = np.trapz(self.file[2].data, velocity, axis=0)
+    self.species_intensity_integrated = np.trapz(self.file[1].data, self.velocity, axis=0)
+    self.dust_intensity_integrated = np.trapz(self.file[2].data, self.velocity, axis=0)
     self.species = self.file[1].header['SPECIES'].split(', ')
     self.species_length = len(self.species)
     self.dust = self.file[2].header['Dust'].split(', ')
-    self.fig = fig
+    self.fig = plt.figure(figsize=(9, 6))
     self.initUI()
   
   def initUI(self):
@@ -57,23 +62,23 @@ class viewMap(QWidget):
     
     QMainWindow().setCentralWidget(QWidget())
     
-    self.setLayout(QVBoxLayout())
-    self.layout().setContentsMargins(0, 0, 0, 0)
-    self.layout().setSpacing(0)
+    self.window.setLayout(QVBoxLayout())
+    self.window.layout().setContentsMargins(0, 0, 0, 0)
+    self.window.layout().setSpacing(0)
     
     self.canvas = FigCanvas(self.fig)
     self.canvas.draw()
     
-    scroll = QScrollArea(self)
+    scroll = QScrollArea(self.window)
     scroll.setWidget(self.canvas)
     
-    self.VELslider = QSlider(Qt.Horizontal, self)
+    self.VELslider = QSlider(Qt.Horizontal, self.window)
     self.VELslider.setRange(0, self.file[1].shape[0] - 1)
     self.VELslider.valueChanged[int].connect(self.changeVelocity)
     
-    self.VEL = QLabel('Velocity: {}'.format(0))
+    self.VEL = QLabel('Velocity: {:.2f}'.format(self.velocity[0]))
     
-    self.SPEslider = QSlider(Qt.Horizontal, self)
+    self.SPEslider = QSlider(Qt.Horizontal, self.window)
     self.SPEslider.setRange(0, len(self.species) + len(self.dust) - 1)
     self.SPEslider.valueChanged[int].connect(self.changeSpecies)
     
@@ -82,15 +87,15 @@ class viewMap(QWidget):
     button = QPushButton('Integrate')
     button.clicked.connect(self.plotIntegrated)
     
-    nav = NabToolbar(self.canvas, self)
+    nav = NabToolbar(self.canvas, self.window)
     
-    self.layout().addWidget(nav)
-    self.layout().addWidget(scroll)
-    self.layout().addWidget(self.VELslider)
-    self.layout().addWidget(self.VEL)
-    self.layout().addWidget(self.SPEslider)
-    self.layout().addWidget(self.SPE)
-    self.layout().addWidget(button)
+    self.window.layout().addWidget(nav)
+    self.window.layout().addWidget(scroll)
+    self.window.layout().addWidget(self.VELslider)
+    self.window.layout().addWidget(self.VEL)
+    self.window.layout().addWidget(self.SPEslider)
+    self.window.layout().addWidget(self.SPE)
+    self.window.layout().addWidget(button)
     self.updatePlot(self.file[1].data[0,:,:,0], '{}'.format(self.species[0]))
     
     self.show_basic()
@@ -101,7 +106,7 @@ class viewMap(QWidget):
     # Update the subplot value.
     
     spe = self.SPEslider.value()
-    self.VEL.setText('Velocity: {}'.format(value))
+    self.VEL.setText('Velocity: {:.2f}'.format(self.velocity[value]))
     
     if spe<self.species_length:
       self.updatePlot(self.file[1].data[value,:,:,spe], '{}'.format(self.species[spe]))
@@ -144,15 +149,21 @@ class viewMap(QWidget):
     for ax in axTemp:
       self.fig.delaxes(ax)
     
-    value = np.full(data.shape, 1e-100)
-    value[data>0] = data[data>0]
-    value = np.log10(value)
+    # value = np.full(data.shape, 1e-100)
+    # value[data>0] = data[data>0]
+    # value = np.log10(value)
     # value = np.nan_to_num(value, nan=-100)
-    vmax = value.max()
+    print(data.shape)
+    vmax = np.ciel(data.max())
+    vmin = np.floor(data.min())
     
-    ax = self.fig.add_axes((0.1, 0.1, 0.8, 0.9))
-    cm = ax.imshow(value, extent=(-np.pi, np.pi, -np.pi/2, np.pi/2), vmin=-2, vmax=max(0,vmax), cmap='cubehelix')
-    cb = fig.colorbar(cm, extend='both', ax=ax, fraction=0.02)
+    ax = self.fig.add_axes((0.1, 0.1, 0.8, 0.9), projection='mollweide')
+    # cm = ax.imshow(data, extent=(-np.pi, np.pi, -np.pi/2, np.pi/2), norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap='cubehelix')
+    lon = np.linspace(-np.pi, np.pi, data.shape[1])
+    lat = np.linspace(-np.pi/2, np.pi/2, data.shape[0])
+    lon,lat = np.meshgrid(lon,lat)
+    cm = ax.pcolormesh(lon, lat, data, norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap='cubehelix')
+    cb = self.fig.colorbar(cm, extend='both', ax=ax, fraction=0.02)
     if integrated:
       cb.ax.set_ylabel('Intensity log(K km/s)', fontsize=20, labelpad=16, rotation=270)
     else:
@@ -169,9 +180,9 @@ class viewMap(QWidget):
   def show_basic(self):
     # Set window geometry and show on screen.
     
-    self.setWindowTitle(self.title)
-    self.setGeometry(*self.posXY, *self.windowSize)
-    self.show()
+    self.window.setWindowTitle(self.title)
+    self.window.setGeometry(*self.posXY, *self.windowSize)
+    self.window.show()
     
     return
 
