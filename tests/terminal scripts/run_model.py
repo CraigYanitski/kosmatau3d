110f6ae263bd6f53c0883fb76726d76a780e4321
yanitski <yanitski@ph1.uni-koeln.de>
2021-03-10 00:56:30 +0100

    [update] adjusted the method calls to the new multiprocessing implementation, even the Windows child process setup; Windows functionality cannot work; will test on Linux soon

--- tests/terminal scripts/run_model.py
+++ tests/terminal scripts/run_model.py
@@ -6,8 +6,7 @@ from astropy.io import fits
 
 from kosmatau3d import models
 
-timed = False
-debug = False
+radiativeTransfer = False
 
 # print('KOSMA-tau^3')
 
@@ -40,15 +39,41 @@ parameters = {
                 'globalUV': 10
               }
 
+if ('win' in sys.platform) and (__name__!='__main__') and not radiativeTransfer:
+  models.constants.type = parameters['modelType']  # this just adds a label to the type of model being created. ie 'disk', 'bar', 'sphere', etc.
+  models.constants.voxel_size = float(parameters['resolution'])
+  models.constants.HISTORYPATH = parameters['history_path']
+  models.constants.changeDirectory(parameters['directory'])
+  
+  # Factors
+  models.constants.clumpMassFactor = parameters['clumpMassFactor']
+  models.constants.FUVFactor = parameters['FUVfactor']
+  models.constants.densityFactor = parameters['densityFactor']
+  models.constants.globalUV = parameters['globalUV']
+  
+  models.constants.changeVelocityRange(parameters['velocityRange'])
+  models.constants.changeVelocityNumber(parameters['velocityNumber'])
+  models.constants.addClumps(massRange=parameters['clumpMassRange'], num=parameters['clumpMassNumber'], Nmax=parameters['clumpNmax'], reset=True)
+  
+  models.observations.methods.initialise()
+  models.species.addMolecules(parameters['molecules'])
+  models.constants.changeDustWavelengths(parameters['dust'])
+
 if __name__=='__main__':
+  timed = False
+  debug = False
+  multiprocessing = 0
+  
   kosma = models.Model(**parameters)
   
-  kosma.calculateModel(timed=timed, debug=debug)
+  kosma.calculateModel(timed=timed, debug=debug, multiprocessing=multiprocessing)
+  
+  radiativeTransfer = True
 
 directory = parameters['history_path'] + parameters['directory'] + r'\r{}_cm{}-{}_d{}_uv{}'.format(parameters['resolution'], parameters['clumpMassFactor'][0], parameters['clumpMassFactor'][1], parameters['densityFactor'], parameters['globalUV'])
 
 # Edit the directory in this condition to allow multiprocessing in Windows. Linux does not need this hard encoding.
-if ('win' in sys.platform) and (__name__!='__main__'):
+if ('win' in sys.platform) and (__name__!='__main__') and radiativeTransfer:
   
   models.radiativeTransfer.voxelPositions = fits.open(directory+'/voxel_position.fits', mode='denywrite')
   models.radiativeTransfer.voxelVelocities = fits.open(directory+'/voxel_velocity.fits', mode='denywrite')
@@ -58,4 +83,7 @@ if ('win' in sys.platform) and (__name__!='__main__'):
   models.radiativeTransfer.tempDustAbsorption = fits.open(directory+'/dust_absorption.fits', mode='denywrite')# in 1/pc
   
 if __name__=='__main__':
-  models.radiativeTransfer.calculateObservation(directory=directory, dim='spherical', slRange=[(-np.pi,np.pi), (-np.pi/8,np.pi/8)], nsl=[360,45], terminal=True, debug=False, multiprocessing=4)
+  multiprocessing = 4
+  
+  models.radiativeTransfer.calculateObservation(directory=directory, dim='spherical', slRange=[(-np.pi,np.pi), (-np.pi/8,np.pi/8)], \
+                                                nsl=[360,45], terminal=True, debug=False, multiprocessing=multiprocessing)
