600695535dbb35ef90729ba31430d82181e005cb
yanitski <yanitski@ph1.uni-koeln.de>
2021-03-17 00:11:59 +0100

    [update] improved plotting for PV option; min/max set in spinBoxes; need to improve event handling

--- kosmatau3d/models/plotting/viewMap.py
+++ kosmatau3d/models/plotting/viewMap.py
@@ -12,7 +12,9 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
   QApplication,
   QMainWindow,
+  QHBoxLayout,
   QVBoxLayout,
+  QGridLayout,
   QWidget,
   QLabel,
   QPushButton,
@@ -20,6 +22,7 @@ from PyQt5.QtWidgets import (
   QCheckBox,
   QScrollArea,
   QSlider,
+  QDoubleSpinBox,
 )
 from matplotlib.backends.backend_qt5agg import (
   FigureCanvasQTAgg as FigCanvas,
@@ -42,12 +45,12 @@ def main():
 
 class viewMap(QApplication):
   
-  def __init__(self, directory='', file='/channel_intensity.fits', posXY=(700, 40), windowSize=(925, 750), cmap='cubehelix'):
+  def __init__(self, directory='', file='/channel_intensity.fits', posXY=(700, 40), windowSize=(925, 850), cmap='cubehelix', cmappv='jet'):
     if not directory:
       print('please specify the directory of the model history files.')
     super().__init__(sys.argv)
     self.window = QWidget()
-    self.title = 'Subplot Switcher'
+    self.title = 'Channel Map Viewer'
     self.posXY = posXY
     self.windowSize = windowSize
     self.directory = directory
@@ -71,6 +74,7 @@ class viewMap(QApplication):
     self.dust = self.file[2].header['Dust'].split(', ')
     self.fig = plt.figure(figsize=(9, 6))
     self.cmap = cmap
+    self.cmappv = cmappv
     self.initUI()
   
   def initUI(self):
@@ -79,6 +83,7 @@ class viewMap(QApplication):
     QMainWindow().setCentralWidget(QWidget())
     # print(self.window.styleSheet())
     
+    # Setup a scrollable canvas for the plot
     self.window.setLayout(QVBoxLayout())
     self.window.layout().setContentsMargins(0, 0, 0, 0)
     self.window.layout().setSpacing(0)
@@ -89,11 +94,29 @@ class viewMap(QApplication):
     scroll = QScrollArea(self.window)
     scroll.setWidget(self.canvas)
     
+    # Min/max boxes for the intensity colour map
+    self.minIlabel = QLabel('minimum I:')
+    self.minI = QDoubleSpinBox()
+    self.minI.setMaximum(1.0001)
+    self.minI.setMinimum(-9.9999)
+    self.minI.setSingleStep(0.0001)
+    self.minI.setDecimals(4)
+    self.minI.setValue(0.01)
+    self.minI.valueChanged.connect(self.plotIntegrated)
+    self.maxIlabel = QLabel('maximum I:')
+    self.maxI = QDoubleSpinBox()
+    self.maxI.setMaximum(999.9999)
+    self.maxI.setMinimum(1.0001)
+    self.maxI.setSingleStep(0.0001)
+    self.maxI.setDecimals(4)
+    self.maxI.setValue(10)
+    self.maxI.valueChanged.connect(self.plotIntegrated)
+    
     # Velocity slider (only for channel maps)
     self.VELslider = QSlider(Qt.Horizontal, self.window)
     self.VELslider.setRange(0, self.velocity.size - 1)
     self.VELslider.valueChanged[int].connect(self.changeVelocity)
-    self.VEL = QLabel('Velocity: {:.2f}'.format(self.velocity[0]))
+    self.VEL = QLabel('Velocity: {:.2f} km/s'.format(self.velocity[0]))
     
     # Species/dust wavelength slider
     self.SPEslider = QSlider(Qt.Horizontal, self.window)
@@ -109,8 +132,9 @@ class viewMap(QApplication):
     self.LATslider.setEnabled(False)
     self.LAT = QLabel('Latitude: {:.1f} degrees'.format(self.latitude[self.LATslider.value()]*180/np.pi))
     
-    # Button to plot integrated intensity
+    # Button to plot channel intensity (enabled by default)
     self.channelButton = QRadioButton('Channel intensity')
+    self.channelButton.toggle()
     self.channelButton.clicked.connect(self.changeVelocity)
     
     # Button to plot integrated intensity
@@ -122,22 +146,62 @@ class viewMap(QApplication):
     self.PVButton.clicked.connect(self.plotPV)
     
     # Button to plot position-velocity diagram
-    self.tempButton = QRadioButton('Print window size')
+    self.tempButton = QPushButton('Print window size')
     self.tempButton.clicked.connect(self.printSize)
     
     nav = NabToolbar(self.canvas, self.window)
-    
     self.window.layout().addWidget(nav)
     self.window.layout().addWidget(scroll)
-    self.window.layout().addWidget(self.VELslider)
-    self.window.layout().addWidget(self.VEL)
-    self.window.layout().addWidget(self.SPEslider)
-    self.window.layout().addWidget(self.SPE)
-    self.window.layout().addWidget(self.LATslider)
-    self.window.layout().addWidget(self.LAT)
-    self.window.layout().addWidget(self.channelButton)
-    self.window.layout().addWidget(self.integrateButton)
-    self.window.layout().addWidget(self.PVButton)
+    
+    grid = QGridLayout()
+    layout = QGridLayout()
+    layout.addWidget(self.minIlabel, 0, 0)
+    layout.addWidget(self.minI, 0, 1)
+    layout.addWidget(self.maxIlabel, 1, 0)
+    layout.addWidget(self.maxI, 1, 1)
+    grid.addLayout(layout, 0, 0, 2, 2)
+    grid.addWidget(self.VEL, 2, 0)
+    grid.addWidget(self.VELslider, 2, 1, 1, 6)
+    grid.addWidget(self.SPE, 3, 0)
+    grid.addWidget(self.SPEslider, 3, 1, 1, 6)
+    grid.addWidget(self.LAT, 4, 0)
+    grid.addWidget(self.LATslider, 4, 1, 1, 6)
+    layout = QHBoxLayout()
+    layout.addWidget(self.channelButton)
+    layout.addWidget(self.integrateButton)
+    layout.addWidget(self.PVButton)
+    # layout.addWidget(self.tempButton)
+    grid.addLayout(layout, 5, 0, 1, 7)
+    self.window.layout().addLayout(grid)
+    
+    # column = QVBoxLayout()
+    # column.addWidget(nav)
+    # column.addWidget(scroll)
+    # column.addWidget(self.VELslider)
+    # column.addWidget(self.VEL)
+    # column.addWidget(self.SPEslider)
+    # column.addWidget(self.SPE)
+    # column.addWidget(self.LATslider)
+    # column.addWidget(self.LAT)
+    # layout = QHBoxLayout()
+    # layout.addWidget(self.channelButton)
+    # layout.addWidget(self.integrateButton)
+    # layout.addWidget(self.PVButton)
+    # layout.addWidget(self.tempButton)
+    # column.addLayout(layout)
+    # self.window.layout().addLayout(column)
+    
+    # self.window.layout().addWidget(nav)
+    # self.window.layout().addWidget(scroll)
+    # self.window.layout().addWidget(self.VELslider)
+    # self.window.layout().addWidget(self.VEL)
+    # self.window.layout().addWidget(self.SPEslider)
+    # self.window.layout().addWidget(self.SPE)
+    # self.window.layout().addWidget(self.LATslider)
+    # self.window.layout().addWidget(self.LAT)
+    # self.window.layout().addWidget(self.channelButton)
+    # self.window.layout().addWidget(self.integrateButton)
+    # self.window.layout().addWidget(self.PVButton)
     # self.window.layout().addWidget(self.tempButton)
     self.updatePlot(self.file[1].data[0,:,:,0], '{}'.format(self.species[0]))
     
@@ -147,6 +211,7 @@ class viewMap(QApplication):
   
   def printSize(self, value):
     print(value)
+    print(self.window.sizeHint())
     print('height:', self.window.height())
     print('width:', self.window.width())
     return
@@ -157,12 +222,15 @@ class viewMap(QApplication):
     if self.integrateButton.isChecked()==True:
       self.plotIntegrated()
       return
+    elif self.channelButton.isChecked()==True:
+      self.VELslider.setEnabled(True)
+      self.LATslider.setEnabled(False)
     
     if value==True:
       value = self.VELslider.value()
     
     spe = self.SPEslider.value()
-    self.VEL.setText('Velocity: {:.2f}'.format(self.velocity[value]))
+    self.VEL.setText('Velocity: {:.2f} km/s'.format(self.velocity[value]))
     
     if spe<self.species_length:
       self.updatePlot(self.file[1].data[value,:,:,spe], '{}'.format(self.species[spe]))
@@ -181,6 +249,9 @@ class viewMap(QApplication):
     elif self.integrateButton.isChecked()==True:
       self.plotIntegrated()
       return
+    else:
+      self.VELslider.setEnabled(True)
+      self.LATslider.setEnabled(False)
     
     if value==True:
       value = self.SPEslider.value()
@@ -211,6 +282,7 @@ class viewMap(QApplication):
       return
     else:
       self.VELslider.setEnabled(False)
+      self.LATslider.setEnabled(False)
     
     if spe<self.species_length:
       self.SPE.setText('Species: {}'.format(self.species[spe]))
@@ -236,8 +308,8 @@ class viewMap(QApplication):
       self.VELslider.setEnabled(False)
       self.LATslider.setEnabled(True)
       
-    if value==True:
-      value = self.LATslider.value()
+    # if value==True:
+    value = self.LATslider.value()
     self.LAT.setText('Latitude: {:.1f} degrees'.format(self.latitude[value]*180/np.pi))
     
     if spe<self.species_length:
@@ -257,24 +329,30 @@ class viewMap(QApplication):
     for ax in axTemp:
       self.fig.delaxes(ax)
     
-    vmax = np.max(data)
-    vmin = np.min(data)
+    # vmax = np.max(data)
+    # vmin = np.min(data)
+    vmin = self.minI.value()
+    vmax = self.maxI.value()
     
     if PV:
       data = copy(data)
       data[data==0] = np.nan
-      vmax = np.nanmax(data)
-      vmin = np.nanmin(data)
+      # vmax = np.nanmax(data)
+      # vmin = np.nanmin(data)
       ax = self.fig.add_axes((0.1, 0.1, 0.8, 0.9))
-      cm = ax.pcolormesh(self.lonPV*180/np.pi, self.velPV, data, norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap=self.cmap)
+      cm = ax.pcolormesh(self.lonPV*180/np.pi, self.velPV, data, norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap=self.cmappv)
     else:
       ax = self.fig.add_axes((0.1, 0.1, 0.8, 0.9), projection='mollweide')
       cm = ax.pcolormesh(self.lonGrid, self.latGrid, data, norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap=self.cmap)
     # cm = ax.imshow(data, extent=(-np.pi, np.pi, -np.pi/2, np.pi/2), norm=colors.SymLogNorm(linthresh=0.1, vmin=vmin, vmax=vmax), cmap='cubehelix')
     cb = self.fig.colorbar(cm, ax=ax, fraction=0.02)
     if integrated:
+      self.minIlabel.setText('minimum I km/s:')
+      self.maxIlabel.setText('maximum I km/s:')
       cb.ax.set_ylabel(r'Intensity $log_{10} \left( K \frac{km}{s} \right)$', fontsize=20, labelpad=16, rotation=270)
     else:
+      self.minIlabel.setText('minimum I:')
+      self.maxIlabel.setText('maximum I:')
       cb.ax.set_ylabel(r'Intensity $log_{10} \left( K \right)$', fontsize=20, labelpad=16, rotation=270)
     ax.set_xlabel(r'Longitude $\left( rad \right)$', fontsize=16)
     if PV:
