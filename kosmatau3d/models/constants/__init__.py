[update] fixed value of chi_0 to convert the FUV distribution to the appropriate units; inclusion of some constants defined in Model(); corrected interpolation of FUV field to correspond to the same wavelength range

kosmatau3d/models/constants/__init__.py
@@ -91,8 +91,9 @@ clumpNmax = [1, 100]
 clumpDensity = [None, 1911]  # use None to take the voxel density
 # clumpFUV = [None, 10]  #use None to take the voxel FUV field
 
+interclump_fillingfactor = None
 interclumpDensity = 1911  # as defined in the old version of this code
-interclumpLogFUV = 1
+interclumpLogFUV = None
 interclumpMassNumber = 1
 interclumpLogMassRange = [-2]
 interclumpLogMass = np.linspace(interclumpLogMassRange[0], interclumpLogMassRange[-1], num=interclumpMassNumber)
@@ -119,7 +120,8 @@ massLimits = [-3, 3]
 uvLimits = [0, 6]
 
 # UV adjustment
-normUV = 2.89433*10**39
+u_draine0 = 8.950674e-14 # erg/cm^-3 #2.89433*10**39 # erg/pc^3 #
+normUV = 1 # used to normalise the FUV distribution used
 globalUV = 10
 
 # Interpolation

kosmatau3d/models/interpolations/interpolate.py
@@ -204,10 +204,13 @@ def interpolateFUVfield(verbose=False):
         if verbose:
             print('Creating FUV interpolation')
         fuv = observations.FUVfield
+        lam = np.array([912, 1350, 1500, 1650, 2000, 2200, 2500, 2800, 3650])
+        wav = np.linspace(912, 2066, num=1000)
+        f = interpolate.interp1d(lam, fuv[2], axis=1)
         if constants.interpolation == 'linear':
-            return interpolate.LinearNDInterpolator(fuv[0], fuv[1])
+            return interpolate.LinearNDInterpolator(fuv[0], np.trapz(f(wav), wav))
         elif constants.interpolation == 'cubic' or constants.interpolation == 'radial':
-            return interpolate.Rbf(fuv[0], fuv[1])
+            return interpolate.Rbf(fuv[0][:, 0], fuv[0][:, 1], np.trapz(f(wav), wav))
         else:
             sys.exit('<<ERROR>>: There is no such method as {} to interpolate '.format(interpolation) +
                      'the KOSMA-tau grid.\n\nExitting...\n\n')

kosmatau3d/models/observations/methods.py
@@ -129,7 +129,8 @@ def FUVfield(file='galactic_FUV_complete.dat'):
         fuv = np.loadtxt(constants.INPUTPATH+constants.directory+file)
         # r = np.arange(0,20000,50)
         # fuv = 50/4/np.pi/r**2
-        observations.FUVfield = (fuv[:, :2], constants.FUVFactor*fuv[:, 2], fuv[:, 3:])
+        observations.FUVfield = (fuv[:, :2], fuv[:, 2] * constants.normUV/(constants.pc*100)**3/constants.u_draine0,
+                                 fuv[:, 3:] * constants.normUV/(constants.pc*100)**3/constants.u_draine0)
         # return (r,fuv)
     return
 