387985b9dd8914b3cdcd7d343823e8e4ec6e6dc3
yanitski <yanitski@ph1.uni-koeln.de>
2021-10-27 13:02:43 +0200

    [update] fix kwarg for FUV factor; increase number of transitions included

--- tests/terminal scripts/run_model_grid.py
+++ tests/terminal scripts/run_model_grid.py
@@ -24,7 +24,8 @@ parameters = {
               'resolution': 400,
               # 'molecules': 'all',
               'molecules': ['C+ 1', 'C 1', 'C 2', 'CO 1', 'CO 2', 'CO 3', 'CO 4', 'CO 5', 'CO 6', 'CO 7', 'CO 8',
-                            '13C+ 1', '13C 1', '13C 2', '13CO 1', '13CO 2', '13CO 3', '13CO 4', '13CO 5', 'HCO+ 1'],
+                            '13C+ 1', '13C 1', '13C 2', '13CO 1', '13CO 2', '13CO 3', '13CO 4', '13CO 5', '13CO 6',
+                            '13CO 7', '13CO 8', 'HCO+ 1', 'HCO+ 2', 'HCO+ 3', 'HCO+ 4', 'HCO+ 5'],
               # 'dust': 'PAH',
               'dust': '240um',
               'clumpMassRange': [[0, 2], [-2]],
@@ -37,7 +38,6 @@ parameters = {
               'clumpMassFactor': [1, 1],
               'FUVfactor': 1,
               'densityFactor': 1,
-              'globalUV': 10,
               'r_cmz': 0,
 
               # Logging
@@ -49,20 +49,21 @@ parameters = {
 f_mass1 = 1.0
 f_mass2 = 1.0
 f_density = 1.0
-f_uv = 10
+f_uv = 1
 mass_cl = [0, 2]
 mass_icl = [-2]
 r_cmz = 0
 models.constants.fuv_ism = 2
 
 for resolution in [500]:
+  for f_uv in 10**np.linspace(1.5, 2.5, num=5):
+    for r_cmz in np.arange(0, 3001, 500, dtype=int):
         # for f_mass1 in [0.25, 0.5, 1.0, 2.0, 4.0]:
         #     for f_mass2 in [0.25, 0.5, 1.0, 2.0, 4.0]:
         # if (mass_icl == [-2]) and not (f_mass2 == 4.0):
         #     continue
         # for mass_cl in [[0, 2], [0, 3], [-1, 2], [-1, 3]]:
         #     for mass_icl in [[-2], [-3, -2], [-3, -1], [-2, -1]]:
-        #         for r_cmz in np.arange(2200, 3001, 200, dtype=np.int):
         #             for f_uv in [10, 50, 100]:
         #                 for f_density in [0.5, 1.0, 2.0, 4.0]:
         #                     for f_uv in [10, 100]:
@@ -72,13 +73,14 @@ for resolution in [500]:
         parameters['clumpMassRange'] = [mass_cl, mass_icl]
         parameters['clumpMassNumber'] = [len(mass_cl), len(mass_icl)]
         parameters['densityFactor'] = f_density
-        parameters['globalUV'] = f_uv
+        parameters['FUVfactor'] = f_uv
         parameters['r_cmz'] = r_cmz
-        parameters['folder'] = 'r{}/'.format(int(resolution))
+        parameters['folder'] = 'r{}_rcmz{}_fuv{}/'.format(int(resolution), int(r_cmz), int(f_uv))
 
 
         kosma = models.Model(**parameters)
-        print('    Model {}'.format(models.constants.history))
+        print('\n    -> Model {}'.format(models.constants.history))
+        print('       ' + '-'*len('Model {}'.format(models.constants.history)))
         kosma.calculateModel(multiprocessing=0)
 
         directory = parameters['history_path'] + parameters['directory'] + '/' + parameters['folder']
