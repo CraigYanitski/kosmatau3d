[update] fixed error in the new calculation (there was an unnecessary additional probability factor)

kosmatau3d/models/combinations/combination.py
@@ -50,15 +50,14 @@ def calculateEmission(test_calc=False, test_opacity=False, test_fv=False, f_v=No
       
         for c in combinations.clumpCombination[ens]:
             if test_calc:
-                intensitylist[ens].append((c * (masspoints.clumpIntensity[ens]*(masspoints.clumpOpticalDepth[ens]
-                                                *np.pi*masspoints.clumpRadius[ens].T**2)
+                intensitylist[ens].append((c * (masspoints.clumpIntensity[ens]*(masspoints.clumpOpticalDepth[ens])
                                                 /(1-np.exp(-masspoints.clumpOpticalDepth[ens]))
-                                                /constants.voxel_size**3/f_ds).T).T)
+                                                /constants.voxel_size/f_ds).T).T)
             else:
                 intensitylist[ens].append((c*masspoints.clumpIntensity[ens].T).T)
             if test_opacity:
-                opticaldepthlist[ens].append((c * (masspoints.clumpOpticalDepth[ens]*np.pi
-                                                   *masspoints.clumpRadius[ens].T**2/constants.voxel_size**3/f_ds).T).T)
+                opticaldepthlist[ens].append((c * (masspoints.clumpOpticalDepth[ens]
+                                                   /constants.voxel_size/f_ds).T).T)
             else:
                 opticaldepthlist[ens].append((c*masspoints.clumpOpticalDepth[ens].T).T)
         