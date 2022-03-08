c96f0a6cd3ef4e94f93943c2a9020e416a04b66f
yanitski <yanitski@ph1.uni-koeln.de>
2022-03-08 20:13:50 +0100

    [update] successfully altered the calculation of dust and A_FUV at the combination level to use the total probability; fixed issue when using `test_calc`

--- kosmatau3d/models/combinations/__init__.py
+++ kosmatau3d/models/combinations/__init__.py
@@ -5,8 +5,11 @@ from .combination import *
 from kosmatau3d.models import constants
 
 clumpCombination = [[] for _ in range(len(constants.clumpMassNumber))]
+CLmaxCombination = [[] for _ in range(len(constants.clumpMassNumber))]
 
 clumpIntensity = [[] for _ in range(len(constants.clumpMassNumber))]
 clumpOpticalDepth = [[] for _ in range(len(constants.clumpMassNumber))]
+clumpDustIntensity = [[] for _ in range(len(constants.clumpMassNumber))]
+clumpDustOpticalDepth = [[] for _ in range(len(constants.clumpMassNumber))]
 
 # jit_module(nopython=False)
