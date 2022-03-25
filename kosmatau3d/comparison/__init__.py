ab6eff54ff78f0cb67b372919e7d30a5c78de41d
yanitski <yanitski@ph1.uni-koeln.de>
2022-03-25 19:03:33 +0100

    improve help explanation of submodule

--- kosmatau3d/comparison/__init__.py
+++ kosmatau3d/comparison/__init__.py
@@ -3,5 +3,8 @@ from .model_selection import *
 
 def help():
       print('This submodule is the location of the model comparison methods I wrote to '
-            'compare the kosmatau3d models to observational data.')
+            'compare the kosmatau3d models to observational data. It regrids the '
+            'observations to a common resolution, calculates the observation error, '
+            'and performs multiple goodness-of-fit plots. These can be overall '
+            'model grid likelihood, chi-squared, or line ratios.')
       return
