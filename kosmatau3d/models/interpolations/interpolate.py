fixed typos; mass calculation corrected

kosmatau3d/models/interpolations/interpolate.py
@@ -264,35 +264,36 @@ def calculate_h2_mass(verbose=False, dilled=False):
         if verbose:
             print('Creating clump mass interpolation')
         r_s, sigma_h2 = observations.h2_surface_mass_profile
-        f_sigma = interpolate.interp1d(r_s, sigma_h2, kind='quadratic', 
-                                       fill_value=(sigma_h2[0], sigma_h2[-1]))
+        f_sigma = interpolate.interp1d(r_s.values, sigma_h2.values, kind='quadratic', 
+                                       bounds_error=False, 
+                                       fill_value=(sigma_h2.values[0], sigma_h2.values[-1]))
         r_h, h_h2 = observations.h2_scale_height_profile
-        f_h = interpolate.interp1d(r_h, h_h2, kind='linear', 
+        f_h = interpolate.interp1d(r_h.values, h_h2.values, kind='linear', 
                                    fill_value='extrapolate')
-        r_new = np.linspace(0, constants.rgal, num=100)*1000
+        r_new = np.linspace(0, constants.rgal, num=100)
         half_height = np.sqrt(2*np.log(2))*f_h(r_new)
         rho_0 = f_sigma(r_new)/2/half_height
         i_partial = constants.voxel_size >= 2*half_height
         i_full = constants.voxel_size < 2*half_height
         n_z = np.ceil(half_height/constants.voxel_size*10).astype(int)
-        r_mass = np.hstack(list(r_new for _ in range(n_z+1)))
+        r_mass = np.hstack(list(r_new for _ in range(n_z.max()+1)))
         z_mass = np.hstack(list(np.full_like(r_new, _*0.1*constants.voxel_size, dtype=float) 
-                                for _ in range(n_z+1)))
-        m_mass = list(np.zeros_like(r_new, dtype=float) for _ in range(n_z+1))
-        for _ in range(n_z+1):
+                                for _ in range(n_z.max()+1)))
+        m_mass = list(np.zeros_like(r_new, dtype=float) for _ in range(n_z.max()+1))
+        for _ in range(n_z.max()+1):
             lim_l = (_*0.1-0.5)*constants.voxel_size
             lim_u = (_*0.1+0.5)*constants.voxel_size
             i_full = (lim_l>=-half_height) & (lim_u<=half_height)
             i_partial = (lim_l>=-half_height) & (lim_u>half_height)
             i_all = (lim_l<-half_height) & (lim_u>half_height)
             m_mass[_][i_full] = rho_0[i_full] * constants.voxel_size**3
-            m_mass[_][i_partial] = rho_0[i_partial] * constants.voxel_size**2 * (half_height-lim_l)
-            m_mass[_][i_all] = rho_0[i_all] * constants.voxel_size**2 * 2*half_height
+            m_mass[_][i_partial] = rho_0[i_partial] * constants.voxel_size**2 * (half_height[i_partial]-lim_l)
+            m_mass[_][i_all] = rho_0[i_all] * constants.voxel_size**2 * 2*half_height[i_all]
         interpolations.h2_mass_full = pd.DataFrame(data={'r':r_mass, 
                                                          'z':z_mass, 
                                                          'M':np.hstack(m_mass)})
-        return interpolate.LinearNDInterpolator(np.array(interpolations.h2_mass_full.r, 
-                                                         interpolations.h2_mass_full.z),
+        return interpolate.LinearNDInterpolator(np.array([interpolations.h2_mass_full.r, 
+                                                          interpolations.h2_mass_full.z]).T,
                                                 interpolations.h2_mass_full.M)
         # if constants.interpolation == 'linear':
         #     return interpolate.LinearNDInterpolator(np.array([clumpMass.R, clumpMass.Z]), 
@@ -320,34 +321,35 @@ def calculate_hi_mass(like_clumps=False, verbose=False, dilled=True):
         else:
             r_s, sigma_hi = observations.hi_surface_mass_profile
             r_h, h_hi = observations.hi_scale_height_profile
-        f_sigma = interpolate.interp1d(r_s, sigma_hi, kind='quadratic', 
-                                       fill_value=(sigma_hi[0], sigma_hi[-1]))
-        f_h = interpolate.interp1d(r_h, h_hi, kind='linear', 
+        f_sigma = interpolate.interp1d(r_s.values, sigma_hi.values, kind='quadratic', 
+                                       bounds_error=False, 
+                                       fill_value=(sigma_hi.values[0], sigma_hi.values[-1]))
+        f_h = interpolate.interp1d(r_h.values, h_hi.values, kind='linear', 
                                    fill_value='extrapolate')
-        r_new = np.linspace(0, constants.rgal, num=100)*1000
+        r_new = np.linspace(0, constants.rgal, num=100)
         half_height = np.sqrt(2*np.log(2))*f_h(r_new)
         rho_0 = f_sigma(r_new)/2/half_height
         i_partial = constants.voxel_size >= 2*half_height
         i_full = constants.voxel_size < 2*half_height
         n_z = np.ceil(half_height/constants.voxel_size*10).astype(int)
-        r_mass = np.hstack(list(r_new for _ in range(n_z+1)))
+        r_mass = np.hstack(list(r_new for _ in range(n_z.max()+1)))
         z_mass = np.hstack(list(np.full_like(r_new, _*0.1*constants.voxel_size, dtype=float) 
-                                for _ in range(n_z+1)))
-        m_mass = list(np.zeros_like(r_new, dtype=float) for _ in range(n_z+1))
-        for _ in range(n_z+1):
+                                for _ in range(n_z.max()+1)))
+        m_mass = list(np.zeros_like(r_new, dtype=float) for _ in range(n_z.max()+1))
+        for _ in range(n_z.max()+1):
             lim_l = (_*0.1-0.5)*constants.voxel_size
             lim_u = (_*0.1+0.5)*constants.voxel_size
             i_full = (lim_l>=-half_height) & (lim_u<=half_height)
             i_partial = (lim_l>=-half_height) & (lim_u>half_height)
             i_all = (lim_l<-half_height) & (lim_u>half_height)
             m_mass[_][i_full] = rho_0[i_full] * constants.voxel_size**3
-            m_mass[_][i_partial] = rho_0[i_partial] * constants.voxel_size**2 * (half_height-lim_l)
-            m_mass[_][i_all] = rho_0[i_all] * constants.voxel_size**2 * 2*half_height
+            m_mass[_][i_partial] = rho_0[i_partial] * constants.voxel_size**2 * (half_height[i_partial]-lim_l)
+            m_mass[_][i_all] = rho_0[i_all] * constants.voxel_size**2 * 2*half_height[i_all]
         interpolations.hi_mass_full = pd.DataFrame(data={'r':r_mass, 
                                                          'z':z_mass, 
                                                          'M':np.hstack(m_mass)})
-        return interpolate.LinearNDInterpolator(np.array(interpolations.hi_mass_full.r, 
-                                                         interpolations.hi_mass_full.z),
+        return interpolate.LinearNDInterpolator(np.array([interpolations.hi_mass_full.r, 
+                                                          interpolations.hi_mass_full.z]).T,
                                                 interpolations.hi_mass_full.M)
         # if constants.interpolation == 'linear':
         #     return interpolate.LinearNDInterpolator(np.asarray([interclumpMass.R, interclumpMass.Z]), 

kosmatau3d/models/observations/methods.py
@@ -28,15 +28,19 @@ def initialise_grid(tau_grid_file='clump_tau_LineCenter.dat',
     return
 
 
-def initialise_input(h2_mass_file='mass_profile.dat', 
-                     hi_mass_file='mass_profile_inter.dat', 
-                     density_file='densities_clouds.dat', 
+def initialise_input(h2_surface_density_file='h2_surface-density.dat', 
+                     hi_surface_density_file='hi_surface-density.dat', 
+                     h2_scale_height_file='h2_scale-height.dat', 
+                     hi_scale_height_file='hi_scale-height.dat', 
+                     h_number_density_file='h_number-density.dat', 
                      fuv_file='galactic_FUV_complete.dat', 
                      velocity_file='rot_milki2018_14.dat'):
     # Model
-    h2_surface__mass_profile(h2_mass_file)
-    hi_surface_mass_profile(hi_mass_file)
-    number_density_profile(density_file)
+    h2_surface_mass_profile(h2_surface_density_file)
+    hi_surface_mass_profile(hi_surface_density_file)
+    h2_scale_height_profile(h2_scale_height_file)
+    hi_scale_height_profile(hi_scale_height_file)
+    number_density_profile(h_number_density_file)
     fuv_profile(fuv_file)
     galaxy_rotation_profile(velocity_file)
 
@@ -113,7 +117,8 @@ def e_tilde_imaginary(file='Eimag.dat'):
 def h2_surface_mass_profile(file='h2_surface-density.dat'):
     # Open file for the mass profile (clump) of the object (Msol/pc**2)
     if constants.directory != '':
-        sigma_h2 = pd.read_csv(constants.INPUTPATH+constants.directory+file, sep=' ')
+        sigma_h2 = pd.read_csv(constants.INPUTPATH+constants.directory+file, 
+                               delim_whitespace=True, skiprows=1)
         # if constants.voxel_size > 2*constants.scale_height_h2:
         #     r = np.hstack((clumpMass['r'] * 1000, clumpMass['r'] * 1000))
         #     z = np.hstack((np.zeros_like(clumpMass['r'] * 1000, dtype=float), 
@@ -142,7 +147,8 @@ def h2_surface_mass_profile(file='h2_surface-density.dat'):
 def hi_surface_mass_profile(file='hi_surface-density.dat'):
     # Open file for the mass profile (interclump) of the object (Msol/pc**2)
     if constants.directory != '':
-        sigma_hi = pd.read_csv(constants.INPUTPATH+constants.directory+file, sep=' ')
+        sigma_hi = pd.read_csv(constants.INPUTPATH+constants.directory+file, 
+                               delim_whitespace=True, skiprows=1)
         # if constants.voxel_size > 2*constants.scale_height_hi:
         #     r = np.hstack((interclumpMass['r'] * 1000, interclumpMass['r'] * 1000))
         #     z = np.hstack((np.zeros_like(interclumpMass['r'] * 1000, dtype=float), 
@@ -187,7 +193,7 @@ def hi_scale_height_profile(file='hi_scale-height.dat'):
     return
 
 
-def number_density_profile(file='densities_clouds.dat'):
+def number_density_profile(file='number-densities.dat'):
     # Open file for the number density profile of the object (n/cm**3)
     if constants.directory != '':
         density = np.genfromtxt(constants.INPUTPATH+constants.directory+file, 