fixed issue in interclump column density calculation

kosmatau3d/models/interpolations/interpolate.py
@@ -28,7 +28,7 @@ def initialise_grid(dilled=False):
     interpolations.clump_taufuv_interpolation = calculate_clump_taufuv(dilled=dilled)
     interpolations.interclump_taufuv_interpolation = calculate_interclump_taufuv(dilled=dilled)
     interpolations.clump_column_density_interpolation = calculate_clump_column_density(dilled=dilled)
-    interpolations.interclump_column_density_interpolation = calculate_clump_column_density(dilled=dilled)
+    interpolations.interclump_column_density_interpolation = calculate_interclump_column_density(dilled=dilled)
     interpolations.clump_hi_column_density_interpolation = calculate_clump_hi_column_density(dilled=dilled)
     interpolations.interclump_hi_column_density_interpolation = calculate_interclump_hi_column_density(dilled=dilled)
     interpolations.clump_h2_column_density_interpolation = calculate_clump_h2_column_density(dilled=dilled)
@@ -392,7 +392,6 @@ def calculate_interclump_column_density(verbose=False, dilled=False):
     if verbose:
         print('Creating N(H) grid interpolation')
     nmchi, N = observations.interclump_column_density
-    print('\n\n', nmchi.n.min(), '\n\n')
     nmchi = nmchi/10.
     log_N = np.log10(N)
     column_densities = []