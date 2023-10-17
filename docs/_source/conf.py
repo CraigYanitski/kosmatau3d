3a8fdcae4ddab772d21a203762ae69afe707c68b
yanitski <yanitski@ph1.uni-koeln.de>
2023-10-17 15:41:44 +0200

    removed unnecessary extension

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
     'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
-    'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
+    # 'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
     'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
     'IPython.sphinxext.ipython_console_highlighting',
 ]
