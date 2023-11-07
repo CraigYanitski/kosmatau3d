38c525bd35d12e539d443d782aae6f6e419cc045
yanitski <yanitski@ph1.uni-koeln.de>
2023-11-07 09:38:45 +0100

    updated to new version

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../kosmatau3d/'))
+
+sys.path.insert(0, os.path.abspath("../../kosmatau3d/"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'kosmatau3d'
-copyright = '2023, Craig Yanitski'
-author = 'Craig Yanitski'
+project = "kosmatau3d"
+copyright = "2023, Craig Yanitski"
+author = "Craig Yanitski"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.6'
+release = "1.0.7"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,13 +34,13 @@ release = '1.0.6'
 extensions = [
     # 'sphinx.ext.duration',
     # 'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
-    'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",  # Link to other project's documentation (see mapping below)
+    "sphinx.ext.viewcode",  # Add a link to the Python source code for classes, functions etc.
     # 'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
-    'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
-    'IPython.sphinxext.ipython_console_highlighting',
+    "nbsphinx",  # Integrate Jupyter Notebooks and Sphinx
+    "IPython.sphinxext.ipython_console_highlighting",
 ]
 
 # Mappings for sphinx.ext.intersphinx. Projects have to have Sphinx-generated doc! (.inv file)
@@ -47,32 +48,36 @@ extensions = [
 #     "python": ("https://docs.python.org/3/", None),
 # }
 
-autosummary_generate = True   # Turn on sphinx.ext.autosummary
-autoclass_content = 'both'    # Add __init__ doc (params) to class summaries
-html_show_sourcelink = False  # Remove 'view source code' from top of page (for html, not python)
-autodoc_inherit_docstrings = True   # Inherit docstring from bas class if no docstring
-set_type_checking_flag = True   # Enable 'extensive' imports for sphinx_autodoc_typehints
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autoclass_content = "both"  # Add __init__ doc (params) to class summaries
+html_show_sourcelink = (
+    False  # Remove 'view source code' from top of page (for html, not python)
+)
+autodoc_inherit_docstrings = True  # Inherit docstring from bas class if no docstring
+set_type_checking_flag = True  # Enable 'extensive' imports for sphinx_autodoc_typehints
 nbsphinx_allow_errors = True  # Continue through Jupyter errors
-add_module_names = False   # Reemove namespaces from class/method signatures
+add_module_names = False  # Reemove namespaces from class/method signatures
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['../_templates']
+templates_path = ["../_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 # exclude_patterns = ['kosmatau3d/models/cyplot*']
 
+
 def skip_member(app, what, name, obj, skip, options):
     # List of external module names to skip
-    external_modules_to_skip = ['scipy', 'scipy.interpolate']
+    external_modules_to_skip = ["scipy", "scipy.interpolate"]
 
-    if what == 'module' and obj.__name__ in external_modules_to_skip:
+    if what == "module" and obj.__name__ in external_modules_to_skip:
         return True
     return None
 
+
 def setup(app):
-    app.connect('autodoc-skip-member', skip_member)
+    app.connect("autodoc-skip-member", skip_member)
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -80,13 +85,13 @@ def setup(app):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # Default: alabaster
-html_theme = 'pydata_sphinx_theme'
-html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
+html_theme = "pydata_sphinx_theme"
+html_css_files = ["readthedocs-custom.css"]  # Override some CSS settings
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
+html_static_path = ["../_static"]
 
 
 # List of patterns, relative to source directory, that match files and
