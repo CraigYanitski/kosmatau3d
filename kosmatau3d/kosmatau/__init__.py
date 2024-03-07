280e0d37954f8aabbf069e44cc5b1f52f4e2ca9e
yanitski <yanitski@ph1.uni-koeln.de>
2024-03-07 15:34:42 +0100

    updated docstring

--- kosmatau3d/kosmatau/__init__.py
+++ kosmatau3d/kosmatau/__init__.py
@@ -1,4 +1,4 @@
 """
 This is a placeholder for now, but I will eventually place the python 
-implementations of Markus Röllig's KOSMA-:math:`\tau` ensemble routines here.
+implementations of Markus Röllig's KOSMA-:math:`\\tau` ensemble routines here.
 """
